ea6c5652b5bc1f352e327e19432f56d628f6df31
AndresEstan <andrestan1995@gmail.com>
2017-08-23 16:39:41 -0500

    interfaz grafica completa y traducida

---
+++ app/src/main/java/com/andrestan1995gmail/taller1_manillas/Principal.java
@@ -0,0 +1,13 @@
+package com.andrestan1995gmail.taller1_manillas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
